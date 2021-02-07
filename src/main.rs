1aeea2b43ad81baa54ef006371696bcea7d38108
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2021-02-07 22:23:17 +0100

    Enable running of commands in azure yaml files (#1)

--- src/main.rs
+++ src/main.rs
@@ -3,61 +3,63 @@ use std::fs;
 use std::process::Command;
 
 extern crate yaml_rust;
-use yaml_rust::{YamlLoader};
+use yaml_rust::YamlLoader;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-
-    println!("file: {}", filename);
-
-    let contents = fs::read_to_string(filename)
-    .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let docs = YamlLoader::load_from_str(&contents).unwrap();
 
-    // Multi document support, doc is a yaml::Yaml
+    // Multi document support, first_yaml_document is of type yaml::Yaml
     let first_yaml_document = &docs[0];
 
     // Debug support
-    println!("{:?}", first_yaml_document);
+    // println!("{:?}", first_yaml_document);
 
     // azure
     let steps = &first_yaml_document["steps"];
     if steps.as_vec().is_some() {
-        println!("{:?}", steps);
+        // println!("{:?}", steps);
 
         for step in steps.as_vec().unwrap() {
             let shell_script = &step["script"];
+            let working_directory = &step["workingDirectory"];
 
-            println!("{}", shell_script.as_str().unwrap());
+            if shell_script.as_str().is_some() {
+                run_script(shell_script, working_directory)
+            }
         }
     }
 
     //github
     let gh_steps = &first_yaml_document["jobs"]["build"]["steps"];
-    println!("{:?}", gh_steps);
+    // println!("{:?}", gh_steps);
     if gh_steps.as_vec().is_some() {
         for step in gh_steps.as_vec().unwrap() {
             let shell_script = &step["run"];
             let working_directory = &step["working-directory"];
 
             if shell_script.as_str().is_some() {
-                println!("{}", shell_script.as_str().unwrap());
-                let mut cmds: Vec<&str> = shell_script.as_str().unwrap().split(" ").collect();
-                let mut mycmd = Command::new(cmds[0]);
-                let u: Vec<_> = cmds.drain(1..).collect();
-                                for cmd in u {
-                    println!("{}", cmd);
-                    mycmd.arg(cmd);
-                }
-                if working_directory.as_str().is_some() {
-                    mycmd.current_dir(working_directory.as_str().unwrap());
-                }
-                println!("{:#?}", mycmd.output().expect("foo"));
+                run_script(shell_script, working_directory)
             }
         }
     }
+}
+
+fn run_script(shell_script: &yaml_rust::Yaml, working_directory: &yaml_rust::Yaml) {
+    println!("{}", shell_script.as_str().unwrap());
+    let mut shell_script_words: Vec<&str> = shell_script.as_str().unwrap().split(" ").collect();
+    let mut my_command = Command::new(shell_script_words[0]);
+    let command_arguments: Vec<_> = shell_script_words.drain(1..).collect();
+    for argument in command_arguments {
+        my_command.arg(argument);
+    }
+    if working_directory.as_str().is_some() {
+        my_command.current_dir(working_directory.as_str().unwrap());
+    }
+    println!(
+        "{:#?}",
+        my_command.output().expect("Could not execute command")
+    );
 }

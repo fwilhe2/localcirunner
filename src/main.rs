291c711c835a44b34cc9f8fbb57d4c94efd1c021
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-02-08 19:47:14 +0100

    Print display names

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::fs;
+use std::io;
+use std::io::Write;
 use std::process::Command;
 
 extern crate yaml_rust;
@@ -25,9 +27,10 @@ fn main() {
         for step in steps.as_vec().unwrap() {
             let shell_script = &step["script"];
             let working_directory = &step["workingDirectory"];
+            let name = &step["displayName"];
 
             if shell_script.as_str().is_some() {
-                run_script(shell_script, working_directory)
+                run_script(shell_script, name, working_directory)
             }
         }
     }
@@ -39,16 +42,21 @@ fn main() {
         for step in gh_steps.as_vec().unwrap() {
             let shell_script = &step["run"];
             let working_directory = &step["working-directory"];
+            let name = &step["name"];
 
             if shell_script.as_str().is_some() {
-                run_script(shell_script, working_directory)
+                run_script(shell_script, name, working_directory)
             }
         }
     }
 }
 
-fn run_script(shell_script: &yaml_rust::Yaml, working_directory: &yaml_rust::Yaml) {
-    println!("{}", shell_script.as_str().unwrap());
+fn run_script(
+    shell_script: &yaml_rust::Yaml,
+    name: &yaml_rust::Yaml,
+    working_directory: &yaml_rust::Yaml,
+) {
+    // println!("{}", shell_script.as_str().unwrap());
     let shell_script_lines: Vec<&str> = shell_script.as_str().unwrap().split("\n").collect();
     for line in shell_script_lines {
         let mut shell_script_words: Vec<&str> = line.trim().split(" ").collect();
@@ -60,9 +68,22 @@ fn run_script(shell_script: &yaml_rust::Yaml, working_directory: &yaml_rust::Yam
         if working_directory.as_str().is_some() {
             my_command.current_dir(working_directory.as_str().unwrap());
         }
-        println!(
-            "{:#?}",
-            my_command.output().expect("Could not execute command")
-        );
+        // println!(
+        //     "{:#?}",
+        //     my_command.output().expect("Could not execute command")
+        // );
+
+        let output = my_command.output().expect("Could not execute command");
+
+        println!();
+
+        if name.as_str().is_some() {
+            println!("Step: {}", name.as_str().unwrap())
+        }
+        // println!("status: {}", output.status);
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
+
+        assert!(output.status.success());
     }
 }

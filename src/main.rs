6a8859eac236ce588c1c796f767b90ff5401c25c
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2021-02-14 00:01:06 +0100

    Rewrite (#3)

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ use std::process::Command;
 extern crate yaml_rust;
 use yaml_rust::YamlLoader;
 
+// todo:
+// - default environment variables
+// - non-bash shells
+// - make output more pretty
+// - debug mode
+// - allow to configure job for gh workflows
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
@@ -14,75 +21,31 @@ fn main() {
     }
     let filename = &args[1];
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let docs = YamlLoader::load_from_str(&contents).unwrap();
 
-    // Multi document support, first_yaml_document is of type yaml::Yaml
-    let first_yaml_document = &docs[0];
-
-    // Debug support
-    // println!("{:?}", first_yaml_document);
-
-    // azure
-    let steps = &first_yaml_document["steps"];
-    if steps.as_vec().is_some() {
-        // println!("{:?}", steps);
-
-        for step in steps.as_vec().unwrap() {
-            let shell_script = &step["script"];
-            let working_directory = &step["workingDirectory"];
-            let name = &step["displayName"];
-
-            if shell_script.as_str().is_some() {
-                run_script(shell_script, name, working_directory)
-            }
-        }
-    }
-
-    //github
-    let gh_steps = &first_yaml_document["jobs"]["build"]["steps"];
-    // println!("{:?}", gh_steps);
-    if gh_steps.as_vec().is_some() {
-        for step in gh_steps.as_vec().unwrap() {
-            let shell_script = &step["run"];
-            let working_directory = &step["working-directory"];
-            let name = &step["name"];
-
-            if shell_script.as_str().is_some() {
-                run_script(shell_script, name, working_directory)
-            }
-        }
-    }
+    let pipeline = parse_yaml_string(&contents);
+    run_pipeline(pipeline);
 }
 
-fn run_script(
-    shell_script: &yaml_rust::Yaml,
-    name: &yaml_rust::Yaml,
-    working_directory: &yaml_rust::Yaml,
-) {
-    // println!("{}", shell_script.as_str().unwrap());
-    let shell_script_lines: Vec<&str> = shell_script.as_str().unwrap().split("\n").collect();
-    for line in shell_script_lines {
-        let mut shell_script_words: Vec<&str> = shell_line_to_words(line);
-        let mut my_command = Command::new(shell_script_words[0]);
-        let command_arguments: Vec<_> = shell_script_words.drain(1..).collect();
-        for argument in command_arguments {
-            my_command.arg(argument);
-        }
-        if working_directory.as_str().is_some() {
-            my_command.current_dir(working_directory.as_str().unwrap());
-        }
-        // println!(
-        //     "{:#?}",
-        //     my_command.output().expect("Could not execute command")
-        // );
+fn run_pipeline(pipeline: Pipeline) {
+    let commands = pipeline
+        .steps
+        .iter()
+        .map(|step| {
+            shell_command(
+                step.shell_script.to_owned(),
+                step.working_directory.to_owned(),
+            )
+        })
+        .collect::<Vec<Command>>();
 
-        let output = my_command.output().expect("Could not execute command");
+    for mut c in commands {
+        let output = c.output().expect("Could not execute command");
 
         println!();
 
-        if name.as_str().is_some() {
-            println!("Step: {}", name.as_str().unwrap())
-        }
+        // if name.as_str().is_some() {
+        //     println!("Step: {}", name.as_str().unwrap())
+        // }
         // println!("status: {}", output.status);
         io::stdout().write_all(&output.stdout).unwrap();
         io::stderr().write_all(&output.stderr).unwrap();
@@ -91,8 +54,95 @@ fn run_script(
     }
 }
 
-fn shell_line_to_words(line: &str) -> Vec<&str> {
-    line.trim().split(" ").collect()
+fn shell_command(shell_script: String, working_directory: String) -> Command {
+    let mut shell_interpreter = "";
+    if cfg!(windows) {
+        shell_interpreter = "pwsh";
+    } else if cfg!(unix) {
+        shell_interpreter = "bash";
+    }
+
+    let mut my_command = Command::new(shell_interpreter);
+    my_command.arg("-c");
+
+    // fixme is it ok not to split words here?
+    my_command.arg(shell_script);
+
+    if working_directory != "" {
+        my_command.current_dir(working_directory);
+    }
+
+    println!("{:?}", my_command);
+
+    return my_command;
+}
+
+fn parse_yaml_string(yaml: &str) -> Pipeline {
+    let docs = YamlLoader::load_from_str(&yaml).unwrap();
+
+    // Multi document support, first_yaml_document is of type yaml::Yaml
+    let first_yaml_document = &docs[0];
+
+    if first_yaml_document["steps"].is_badvalue() {
+        return github_yaml_to_pipeline(&first_yaml_document["jobs"]);
+    }
+
+    if first_yaml_document["jobs"].is_badvalue() {
+        return azure_yaml_to_pipeline(&first_yaml_document["steps"]);
+    }
+
+    panic!("should not get here, debug info: {:?}", first_yaml_document)
+}
+
+fn azure_yaml_to_pipeline(steps: &yaml_rust::Yaml) -> Pipeline {
+    return Pipeline {
+        steps: steps
+            .as_vec()
+            .unwrap()
+            .iter()
+            .map(|step| Step {
+                name: step["displayName"].as_str().unwrap_or("").to_string(),
+                shell_script: step["script"].as_str().unwrap().to_string(),
+                working_directory: step["workingDirectory"].as_str().unwrap_or("").to_string(),
+            })
+            .collect::<Vec<Step>>(),
+    };
+}
+
+fn github_yaml_to_pipeline(jobs: &yaml_rust::Yaml) -> Pipeline {
+    //fixme job name "build" is hardcoded
+    let steps = &jobs["build"]["steps"];
+
+    return Pipeline {
+        steps: steps
+            .as_vec()
+            .unwrap()
+            .iter()
+            .map(|step| Step {
+                name: step["name"].as_str().unwrap_or("").to_string(),
+                shell_script: step["run"].as_str().unwrap_or("echo nothing").to_string(),
+                working_directory: step["working-directory"].as_str().unwrap_or("").to_string(),
+            })
+            .collect::<Vec<Step>>(),
+    };
+}
+
+#[derive(Debug)]
+pub struct Pipeline {
+    steps: Vec<Step>,
+}
+
+impl PartialEq for Pipeline {
+    fn eq(&self, other: &Self) -> bool {
+        self.steps.len() == other.steps.len()
+    }
+}
+
+#[derive(Debug)]
+pub struct Step {
+    shell_script: String,
+    name: String,
+    working_directory: String,
 }
 
 #[cfg(test)]
@@ -101,7 +151,67 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_shell_line_to_words() {
-        assert_eq!(shell_line_to_words(" this is my line "), vec!["this", "is", "my", "line"]);
+    fn test_pipeline_1() {
+        let input = "jobs:
+    build:
+      steps:
+        - name: configure
+          run: ./configure
+        - name: make
+          run: make";
+        let actual = parse_yaml_string(&input);
+
+        let expected = Pipeline {
+            steps: vec![
+                Step {
+                    shell_script: "./configure".to_string(),
+                    name: "configure".to_string(),
+                    working_directory: "".to_string(),
+                },
+                Step {
+                    shell_script: "make".to_string(),
+                    name: "make".to_string(),
+                    working_directory: "".to_string(),
+                },
+            ],
+        };
+
+        println!("{:#?}", actual);
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_pipeline_2() {
+        let input = "steps:
+  - script: ./configure
+    workingDirectory: abc
+  - script: make
+    displayName: Compile the Code
+  - script: make check";
+        let actual = parse_yaml_string(&input);
+
+        println!("{:#?}", actual);
+
+        let expected = Pipeline {
+            steps: vec![
+                Step {
+                    shell_script: "./configure".to_string(),
+                    name: "".to_string(),
+                    working_directory: "abc".to_string(),
+                },
+                Step {
+                    shell_script: "make".to_string(),
+                    name: "Compile the Code".to_string(),
+                    working_directory: "".to_string(),
+                },
+                Step {
+                    shell_script: "make check".to_string(),
+                    name: "".to_string(),
+                    working_directory: "".to_string(),
+                },
+            ],
+        };
+
+        assert_eq!(actual, expected)
     }
 }
